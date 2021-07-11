316c05643e2a92b87c6d4af441206cdca7d3d0c9
s_labrinos <slabrinos9214@gmail.com>
2021-07-11 11:46:17 +0300

    virual enviromtent created

---
+++ api_keys.py
@@ -0,0 +1,4 @@
+BING_IMAGE_API = "64b3c1bb52024395a6c8316609816168"
+YOU_TUBE_API = "AIzaSyBHIvp_9dsJ3W5z6ICqLzudMCNVkKKONc8"
+RAPID_API_KEY = "af152d27e9msha7147c2e8016f8cp1d056cjsnf177dfbfd6b3"
+DB_CONNECTION = "postgresql://postgres:ms_ais_postgres_2021@snf-879544.vm.okeanos.grnet.gr:5432/csi"
